8f8d0f2c17f7b8111a43f3c8ec83cf96551b9d79
Fran <caviglia80@gmail.com>
2023-07-15 15:11:00 -0300

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { CasosExitoComponent } from './components/casos-exito/casos-exito.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { PreguntasFrecuentesComponent } from './components/preguntas-frecuentes/preguntas-frecuentes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'casos-exito', component: CasosExitoComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'preguntas-frecuentes', component: PreguntasFrecuentesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
