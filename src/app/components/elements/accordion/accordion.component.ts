052909f2d721106d4b59a4d25fbb8b72963472ed
Fran <caviglia80@gmail.com>
2023-07-22 15:37:37 -0300

    .

--- src/app/components/elements/accordion/accordion.component.ts
+++ src/app/components/elements/accordion/accordion.component.ts
@@ -6,27 +6,19 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent implements OnInit {
-  @Input() id: any;
-  @Input() btnStr: any;
-  @Input() body: any;
-  public elementId: string;
-  public elemento: string;
-  public encabezado: string;
-
-  constructor() {
-    this.elementId = '';
-    this.elemento = '';
-    this.encabezado = '';
-  }
+  @Input() id: number = 0;
+  @Input() btnStr: string = '';
+  @Input() body: string = '';
+  public elementId: string = '';
+  public elemento: string = '';
+  public encabezado: string = '';
 
   ngOnInit(): void {
-    this.elementId = '#elemento-' + this.id.toString();
-    this.elemento = 'elemento-' + this.id.toString();
-    this.encabezado = 'encabezado-' + this.id.toString();
+    this.elementId = `#elemento-${this.id}`;
+    this.elemento = `elemento-${this.id}`;
+    this.encabezado = `encabezado-${this.id}`;
     this.btnStr = this.btnStr ? this.btnStr.toString() : '';
     this.body = this.body ? this.body.toString() : '';
   }
-
 }
-
 
