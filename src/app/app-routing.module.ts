6400c63247e10f0dfdce93fb8d5aeffeca42be91
Fran <caviglia80@gmail.com>
2023-07-26 15:50:27 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 /* 
 import { HomeComponent } from '@pages/home/home.component';
 import { ContactoComponent } from '@pages/contacto/contacto.component';
@@ -29,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
