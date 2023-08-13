6392c0ab3a9a7dc903a132918f07a9d6720d3eae
Fran <caviglia80@gmail.com>
2023-08-13 18:57:26 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EstudioJuridico';
+  loaded: boolean = false;
+  exitBack: boolean = false;
+  exit: boolean = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loaded = true;
+      setTimeout(() => {
+        this.exitBack = true;
+        setTimeout(() => {
+          this.exit = true;
+        }, 800);
+      }, 800);
+    }, 250);
+  }
 }
 
 export class GlobalVariables {
